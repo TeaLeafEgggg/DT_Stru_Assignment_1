03078f5606444fbe533500e503270505e5e92ba5
TeaLeafEgggg <chngoscar@gmail.com>
2025-09-25 17:28:48 +0800

    tried something

--- Assignment_1_Part_A.py
+++ Assignment_1_Part_A.py
@@ -1,2 +1,6 @@
-a = 1
-print(a)
+#INT3086 Assignment 1 Part A Self Study
+#Oscar Ng Cheuk Hau 11558129 
+#Shunting Yard Algorithm to convert infix expression to postfix expression
+user_input_items = input("Enter items separated by spaces: ")
+item_list = user_input_items.split()
+print(item_list)
